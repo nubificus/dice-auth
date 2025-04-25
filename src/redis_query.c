9c1b212e84b4be3e9664ce122a891eb59b275698
Ilias Lagomatis <ilago@nubificus.co.uk>
2025-04-25 18:56:59 +0300

    feat: Add extra environment variable for Redis IP
    The name is REDIS_HOST, but the old one still works
    for now.
    
    Signed-off-by: Ilias Lagomatis <ilago@nubificus.co.uk>

--- src/redis_query.c
+++ src/redis_query.c
@@ -4,25 +4,38 @@
 #include <hiredis/hiredis.h>
 #include <redis_query.h>
 
-#define ENV_REDIS_IP "REDIS_SERVICE_SERVICE_HOST"
+#define ENV_REDIS_IP   "REDIS_HOST"
+#define ENV_REDIS_IP_2 "REDIS_SERVICE_SERVICE_HOST"
+
 const char *localhost = "127.0.0.1";
 
-char** redis_get_all_roots(int *nr_certs) {
+char* get_redis_ip() {
 	static char *redis_ip = NULL;
 
 	if (redis_ip == NULL) {
 		redis_ip = getenv(ENV_REDIS_IP);
-		if (redis_ip == NULL) {
-			printf("Could not read Redis IP address from "
-			       "environment (REDIS_SERVICE_SERVICE_HOST)\n"
-			       "Using localhost instead.\n");
-			redis_ip = (char *)localhost;
-		} else {
-			printf("Using Redis IP: %s\n", redis_ip);
+		if (redis_ip != NULL) {
+			printf("Using Redis IP from %s: %s\n", ENV_REDIS_IP, redis_ip);
+			goto out;
 		}
-	}
 
-	redisContext *context = redisConnect(redis_ip, 6379);
+		redis_ip = getenv(ENV_REDIS_IP_2);
+		if (redis_ip != NULL) {
+			printf("Using Redis IP from %s: %s\n", ENV_REDIS_IP_2, redis_ip);
+			goto out;
+		}
+
+		printf("Could not read Redis IP address from"
+		       " environment\n(%s or %s)\nUsing localhost"
+		       " instead.\n", ENV_REDIS_IP, ENV_REDIS_IP_2);
+		redis_ip = (char *)localhost;
+	}
+out:
+	return redis_ip;
+}
+
+char** redis_get_all_roots(int *nr_certs) {
+	redisContext *context = redisConnect(get_redis_ip(), 6379);
 	if (context == NULL || context->err) {
 		if (context) {
 			fprintf(stderr, "Redis connection error: %s\n", context->errstr);
